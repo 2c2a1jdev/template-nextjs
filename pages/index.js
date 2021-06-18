21ffa27bf54ce44d1500000f0e1c317deb0ba5bc
Menueasy <menueasy@gmail.com>
2021-06-18 16:35:12 -0300

    links pessoais - primeiras edições

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,55 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Links - Alan Fernandes</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
+        <div>
+          <Image
+            className={styles.perfilImg} 
+            src="/perfil.jpg" 
+            alt="Imagem de Perfil"
+            width={150}
+            height={150}
+          />
+        </div>
+
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Gratidão pela <a>visita!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Esses são os meus links para contato.
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://api.whatsapp.com/send?1=pt_br&phone=48988491453" target="new" className={styles.card}>
+            <h3>WhatsApp &rarr;</h3>
+            <p>Contato pessoal, responderei  assim que estiver disponível!</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="" target="new" className={styles.card}>
+            <h3>Curriculum &rarr;</h3>
+            <p>Confira minhas experiências profissionais.</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://github.com/2c2a1jdev" target="new" className={styles. card} >
+            <h3>Github &rarr;</h3>
+            <p>Aqui você encontra todos os meus projetos.</p>
           </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
+          <a href="https://www.linkedin.com/in/alanfernandesss/" target="new" className={styles.card} >
+            <h3>Linkedin &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Veja aqui meus contatos profissionais. 
             </p>
           </a>
         </div>
